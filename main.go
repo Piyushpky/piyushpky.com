b50c7b0033a1e34ebec1d51b6164d1f654e54af7
Piyush Kumar <piyushkumar@Piyushs-MacBook-Pro.local>
2024-07-07 15:33:17 +0530

    json

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os" // Import the os package
+	"strconv"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -11,6 +13,14 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/", handler)
-	fmt.Println("Server listening on port 8080...")
-	http.ListenAndServe(":8080", nil)
+
+	// Get port from environment, default to 8080
+	portStr := os.Getenv("PORT")
+	if portStr == "" {
+		portStr = "8080" // Default port if not set
+	}
+	port, _ := strconv.Atoi(portStr)
+
+	fmt.Printf("Server listening on port %d...\n", port)
+	http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
 }
