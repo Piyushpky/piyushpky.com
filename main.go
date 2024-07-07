a7fffc5b20fb63ab0ed2693a0a60a9e34e8e7aae
Piyush Kumar <piyushkumar@Piyushs-MacBook-Pro.local>
2024-07-07 15:10:35 +0530

    end

--- main.go
+++ main.go
@@ -9,8 +9,13 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello World!")
 }
 
+func hHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World! first page")
+}
+
 func main() {
 	http.HandleFunc("/api", helloHandler)
+	http.HandleFunc("/", hHandler)
 	fmt.Println("Server running at http://localhost:8080")
 	http.ListenAndServe(":8080", nil)
 }
